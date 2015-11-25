aae0cfc0b651eef44dcdc75ce7759fa31ad769cb
DeveloperAlex <developeralex@gmail.com>
2015-11-25 12:46:19 -0500

    Moved mongo connection string to passwords.js.  Next make that read from a file.

---
+++ OrdersExpressSvc/passwords.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+var fs = require('fs');  //Eventually read passwords from a file: c:/passwords.json
+
+
+var passwords = {
+    mongolab_com_orders: 'mongodb://ordersUser:o1r2d3e4r5s6U7s8e9r@ds037283.mongolab.com:37283/orders'
+};
+
+module.exports = passwords;
--- OrdersExpressSvc/routes/api.js
+++ OrdersExpressSvc/routes/api.js
@@ -1,8 +1,9 @@
 ﻿"use strict";
 var express = require('express');
 var router = express.Router();
+var passwords = require('../passwords');
 var mongoose = require('mongoose');  //https://mongolab.com/databases/orders
-var db = mongoose.connect('mongodb://ordersUser:o1r2d3e4r5s6U7s8e9r@ds037283.mongolab.com:37283/orders');
+var db = mongoose.connect(passwords.mongolab_com_orders);
 
 var Employee = require('../models/employeeModel.js');
 var Menu = require('../models/menuModel.js');
