714a8202cfc38662ed7ff27daacd542751ec28e0
DeveloperAlex <developeralex@gmail.com>
2015-11-27 14:58:03 -0500

    testing nodemon

--- OrdersExpressSvc/routes/api.js
+++ OrdersExpressSvc/routes/api.js
@@ -115,17 +115,17 @@ router.route('/order/:orderId').get(function (req, res) {
 // CONNECTION EVENTS
 // When successfully connected
 mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection opened');
+    console.log('Mongoose connection opened');
 });
 
 // If the connection throws an error
 mongoose.connection.on('error', function (err) {
-    console.log('Mongoose default connection error: ' + err);
+    console.log('Mongoose connection error: ' + err);
 });
 
 // When the connection is disconnected
 mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
+    console.log('Mongoose connection disconnected');
 });
 
 //// If the Node process ends, close the Mongoose connection
