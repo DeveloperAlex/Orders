c082c2e876c98041355f6ab48e58bd94ee7a532d
DeveloperAlex <developeralex@gmail.com>
2015-11-28 10:35:38 -0500

    Cleaned up app.use's - so they're easier to understand the flow. And some evil improvement.

--- OrdersExpressSvc/server.js
+++ OrdersExpressSvc/server.js
@@ -13,8 +13,9 @@ var api = require('./routes/api');
 
 var app = express();
 app.use(function (req, res, next) {
-    var host = req.headers.host;
-    if (host.indexOf('localhost:8080') === -1 && host.indexOf('developeralex.com:8080') === -1) {
+    //var host = req.headers.host;
+    //if (host.indexOf('localhost:8080') === -1 && host.indexOf('developeralex.com:8080') === -1) {
+    if (isEvil(req)) {
         var fullUrl = req.method + ' ' + host + req.originalUrl;
         console.log('Evil request: ' + fullUrl + ' on ' + new Date().toString());
         res.send('');  //Bad request - stop processing it.
@@ -23,11 +24,19 @@ app.use(function (req, res, next) {
     }
 });
 
+function isEvil(req){
+    var host = req.headers.host;
+    if (host.indexOf('localhost:8080') === -1 && host.indexOf('developeralex.com:8080') === -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -44,42 +53,52 @@ app.use('/', routes);
 app.use('/api', api);
 //app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    //if (true) {
-    //    res.redirect('http://google.com');
-    //    //res.redirect(301, 'http://google.com');
-    //}
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+//// catch 404 and forward to error handler
+//app.use(function (req, res, next) {
+//    //if (true) {
+//    //    res.redirect('http://google.com');
+//    //    //res.redirect(301, 'http://google.com');
+//    //}
+//    
+//    if (!isEvil(req)) {
+//        var err = new Error('Not Found');
+//        err.status = 404;
+//        next(err);
+//    }
+//});
 
+//// development error handler - will print stacktrace
+//if (app.get('env') === 'development') {
+//    app.use(function (err, req, res, next) {
+//        res.status(err.status || 500);
+//        res.render('error', {
+//            message: err.message,
+//            error: err
+//        });
+//    });
+//}
 
-// development error handler - will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        //if (true) {
-        //    res.redirect('http://google.com');
-        //}
+// production error handler - no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    if (!isEvil(req)) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    }
+
+    if (app.get('env') !== 'development' || isEvil(req)) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    } else {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
             error: err
         });
-    });
-}
-
-// production error handler - no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    //if (true) {
-    //    res.redirect('http://google.com');
-    //}
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    }
 });
 
 
