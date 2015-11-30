4135dfd7c81e3a35beb4e2bcbf72c23dd4297ac6
DeveloperAlex <developeralex@gmail.com>
2015-11-29 19:06:19 -0500

    mongo returns data to front-end.

--- OrdersExpressSvc/public/app/feature/login.ctrl.js
+++ OrdersExpressSvc/public/app/feature/login.ctrl.js
@@ -3,14 +3,37 @@
     angular.module('ordersApp').controller('loginController', ['$scope', 'MongoService', function ($scope, MongoService) {
             $scope.logintest = 'Login test 123';
             $scope.mongotest = MongoService.testProperty;
+            
+            $scope.employees = null;
+            //$scope.employees = MongoService.getEmployees();
+            MongoService.getEmployees().then(function (response) {
+                //console.log('MongoService.getEmployees()= ' + response)
+                $scope.employees = response;
+            });
+
+
+
         }]);
     
+
+// ng-repeat
+
     
-    angular.module('ordersApp').directive('myDirective', function () {
-        return {
-            restrict: 'E',
-            template: '<div>This is my directive</div>'
-        };
-    });
+    //angular.module('ordersApp').directive('myDirective', function () {
+    //    return {
+    //        restrict: 'E',
+    //        template: '<div>This is my directive</div>'
+    //    };
+    //});
 
 })();
+
+
+//http://stackoverflow.com/questions/20369377/how-to-use-http-get-in-angularjs-correctly-in-specific-for-an-external-api-cal
+//myApp.controller('AngularJSCtrl', function ($scope, dataService) {
+//    $scope.data = null;
+//    dataService.getData().then(function (response) {
+//        $scope.data = response;
+//    });
+//});
+
--- OrdersExpressSvc/public/app/service/mongo.svc.js
+++ OrdersExpressSvc/public/app/service/mongo.svc.js
@@ -1,16 +1,55 @@
 ﻿(function () { 
-    angular.module("ordersApp").factory('MongoService', function () {
+    angular.module("ordersApp").factory('MongoService', ['$http', '$q', function ($http, $q) {
         var factory = {};
         factory.testProperty = "MongoService - testProperty";
-        
-        factory.method1 = function () {
-            //..
-        };
-        
-        factory.method2 = function () {
-            //..
+            
+        //http://stackoverflow.com/questions/20369377/how-to-use-http-get-in-angularjs-correctly-in-specific-for-an-external-api-cal        
+        factory.getEmployees = function () {
+                deferred = $q.defer();
+                $http({
+                    method: 'GET',
+                    url: 'api/employee'
+                    //url: 'https://www.example.com/api/v1/page'
+                    //params: 'limit=10, sort_by=created:desc',
+                    //headers: { 'Authorization': 'Token token=xxxxYYYYZzzz' }
+                }).success(function (data) {
+                    //console.log('data= ' + data);
+                    deferred.resolve(data);
+                }).error(function () {
+                    alert("error - getEmployees");
+                    deferred.reject(error);
+                });
+                return deferred.promise;
         };
         
         return factory;
-    });
+    }]);
 })();
+
+
+//$http
+
+// Add httpInterceptor to ordersApp.js - with console.log's.  Later we'll add security token thingy - need to research that.
+
+
+
+//myApp.service('dataService', function ($http, $q) {
+//    this.getData = function () {
+//        deferred = $q.defer();
+//        $http({
+//            method: 'GET',
+//            url: 'https://www.example.com/api/v1/page',
+//            params: 'limit=10, sort_by=created:desc',
+//            headers: { 'Authorization': 'Token token=xxxxYYYYZzzz' }
+//        }).success(function (data) {
+//            // With the data succesfully returned, we can resolve promise and we can access it in controller
+//            deferred.resolve();
+//        }).error(function () {
+//            alert("error");
+//            //let the function caller know the error
+//            deferred.reject(error);
+//        });
+//        return deferred.promise;
+//    }
+//});
+
