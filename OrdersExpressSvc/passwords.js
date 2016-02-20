d8858525af523b36afe49da7b6645f8a450db8b9
DeveloperAlex <developeralex@gmail.com>
2016-02-20 16:38:06 -0500

    Ubuntu update

--- OrdersExpressSvc/passwords.js
+++ OrdersExpressSvc/passwords.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 
 try {
     var dir = __dirname;
-    dir = dir.substr(0, dir.lastIndexOf('\\'));
-    dir = dir.substr(0, dir.lastIndexOf('\\'));
-    var file = dir + '\\passwords.json';
+    dir = dir.substr(0, dir.lastIndexOf('/'));
+    dir = dir.substr(0, dir.lastIndexOf('/'));
+    var file = dir + '/passwords.json';
     console.log(new Date().toString());
     console.log('Using passwords file= ' + file);
     var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
