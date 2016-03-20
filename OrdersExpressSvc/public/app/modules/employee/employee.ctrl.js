64139205d60ac4ddcf03f18cf75b763d7278da75
DeveloperAlex <developeralex@gmail.com>
2016-03-20 19:49:21 -0400

    code

--- OrdersExpressSvc/public/app/modules/employee/employee.ctrl.js
+++ OrdersExpressSvc/public/app/modules/employee/employee.ctrl.js
@@ -10,10 +10,8 @@
   /* @ngInject */
   function employeeController($state) {
     var vm = this;
-    //vm.title = 'employee is the title of this';
 
-    vm.employeelist = $state.$current.locals.globals.employeelist
-    //vm.employeelist = employeelist;  //Isn't injected into this controller - from Resolve (in routing).
+    vm.employeelist = $state.$current.locals.globals.employeelist;
 
     vm.reload = function(){
       //$state.reload();
--- OrdersExpressSvc/public/app/modules/employee/employeeDetail.ctrl.js
+++ OrdersExpressSvc/public/app/modules/employee/employeeDetail.ctrl.js
@@ -9,10 +9,7 @@
 
   /* @ngInject */
   function employeeDetailController($state, $stateParams) {
-  //function employeeDetailController() {
     var vm = this;
-    //vm.title = 'employee detail is the title of this';
-    //vm.user = 'blahblahuser';   //$stateParams.user;
     vm.stateParamsUser = $stateParams.user;
 
     vm.resolveduser = $state.$current.locals.globals.resolveduser;
