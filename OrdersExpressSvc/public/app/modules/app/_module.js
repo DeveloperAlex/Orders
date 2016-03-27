c0f858fae83e1581c6ebc6f4b2cfb0d21f479b1b
DeveloperAlex <developeralex@gmail.com>
2016-03-27 00:58:24 -0400

    tweak. Works

--- OrdersExpressSvc/public/app/modules/app/_module.js
+++ OrdersExpressSvc/public/app/modules/app/_module.js
@@ -1,8 +1,41 @@
 //angular.module("ordersApp", ['OTPWeb.Directives', 'ngynSelectKey', 'bento.modern', 'aa.notify', 'ui.router', 'ui.bootstrap', 'angular-data.DSCacheFactory', 'wijmo', 'wijspread']);
 
-angular.module('ordersApp', [
-  'auth0', 'angular-storage', 'angular-jwt',
-  'ui.grid', 'ui.grid.pagination', 'ui.router',
-  'orders.login', 'orders.home',
-  'oa-common', 'oa-employee'
-]); //['ui.bootstrap']);
+//debugger;
+
+// angular.module('ordersApp', [
+//   'auth0', 'angular-storage', 'angular-jwt',
+//   'ui.grid', 'ui.grid.pagination', 'ui.router',
+//   'orders.login', 'orders.home',
+//   'oa-common', 'oa-employee'
+// ]); //['ui.bootstrap']);
+
+//debugger;
+
+// try {
+//   var isAppDefinedYet = angular.module("ordersApp");
+// }
+// catch(err)
+// {
+//   angular.module('ordersApp', [
+//     'auth0', 'angular-storage', 'angular-jwt',
+//     'ui.grid', 'ui.grid.pagination', 'ui.router',
+//     'orders.login', 'orders.home',
+//     'oa-common', 'oa-employee'
+//   ]); //['ui.bootstrap']);
+// }
+
+
+debugger;
+
+var ordersApp = ordersApp;  //|| {};
+
+if (!ordersApp) {
+  ordersApp = angular.module('ordersApp', [
+    'auth0', 'angular-storage', 'angular-jwt',
+    'ui.grid', 'ui.grid.pagination', 'ui.router',
+    'orders.login', 'orders.home',
+    'oa-common', 'oa-employee'
+  ]); //['ui.bootstrap']);
+}
+
+debugger;
