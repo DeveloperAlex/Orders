7926ad83b3e5931b161298d13d7686126de99bd0
DeveloperAlex <developeralex@gmail.com>
2016-03-31 19:42:04 -0400

    Filter on RESTful.   Works

--- OrdersExpressSvc/public/app/modules/app/httpInterceptors.js
+++ OrdersExpressSvc/public/app/modules/app/httpInterceptors.js
@@ -1,28 +1,38 @@
-angular.module('ordersApp')
-  .config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push('oaHttpInterceptor');
+(function () {
+  'use strict';
+
+  angular.module('ordersApp')
+    .config(['$httpProvider', function ($httpProvider) {
+      $httpProvider.interceptors.push('oaHttpInterceptor');
+    }]);
+
+  angular.module('ordersApp').factory('oaHttpInterceptor', ['$q', function ($q) {
+    return {
+      request: function (config) {
+        //TODO: Add token here:
+        // if(token is truthy ...) { config.headers['x-session-token'] = SessionService.token; }
+
+        if (config.url.indexOf('/api/') > -1) {
+          console.log('restful request', config);
+        }
+        return config;
+      },
+
+      response: function (result) {
+        if (result.config.url.indexOf('/api/') > -1) {
+          console.log('restful response', result);
+        }
+        return result;
+      },
+
+      responseError: function (rejection) {
+        //TODO: Show Toastr notification.
+        if (rejection.status == 401) {
+          ;  //Redirect to Login page.
+        }
+        return $q.reject(rejection);
+      }
+    };
   }]);
 
-angular.module('ordersApp').factory('oaHttpInterceptor', ['$q', function($q) {
-  return {
-    request: function(config) {
-      //TODO: Add token here:
-      // if(token is truthy ...) { config.headers['x-session-token'] = SessionService.token; }
-
-      console.log('request config', config);
-      return config;
-    },
-
-    response: function(response) {
-      console.log('response', response);
-      return response;
-    },
-
-    responseError: function(rejection) {
-      if (rejection.status == 401) {
-        ;  //Redirect to Login page.
-      }
-      return $q.reject(rejection);
-    }
-  };
-}]);
+}());
