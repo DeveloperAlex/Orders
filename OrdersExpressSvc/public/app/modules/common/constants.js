a226c02ae533f2eb27388c4c36cd8c18c0155203
DeveloperAlex <developeralex@gmail.com>
2016-04-07 21:18:09 -0500

    Cleaned up client-side RESTful angular service.    Works (tests green).

--- OrdersExpressSvc/public/app/modules/common/constants.js
+++ OrdersExpressSvc/public/app/modules/common/constants.js
@@ -9,8 +9,11 @@
           domain: 'developeralex.auth0.com',
           clientID: 'zK2QYeeXHcR5NsyULkSudRfv3QytCueH'
         },
-        restfulUrl: 'http://app.developeralex.com/api/',  //Suffix url w/ a "/".
-        restfulVersion: 'v1'  //a restful service should be versioned
+        restful: {
+          //url: 'http://app.developeralex.com/api/',  //Suffix url w/ a "/".
+          url: 'http://localhost:8181/api/',  //Suffix url w/ a "/".
+          version: 'v1'  //a restful service should be versioned (either in url or header).
+        }
       });
 
 })();
